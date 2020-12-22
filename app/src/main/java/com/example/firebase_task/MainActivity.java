e69a8414a4588331845c8803be448f3144a752fa
Naitik_Harkhani <naitikharkhani1@gmail.com>
2020-12-22 12:37:27 +0530

    Register form  project link

--- app/src/main/java/com/example/firebase_task/MainActivity.java
+++ app/src/main/java/com/example/firebase_task/MainActivity.java
@@ -13,6 +13,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    //https://www.youtube.com/watch?v=BHT8hCtOP1U
+
     EditText regname, regemail, regpassword, regnumber;
     Button regsignup;
 
